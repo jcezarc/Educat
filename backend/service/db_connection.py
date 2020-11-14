df7df9ec8f618367b0a4e96a986d102e40e0b1e1
Júlio Cascalles <jcezarc@yahoo.com.br>
2020-11-13 21:30:29 -0300

    Bancos de dados MySql e Sqlite

---
+++ backend/service/db_connection.py
@@ -0,0 +1,25 @@
+import os
+from util.db.lite_table import LiteTable
+
+# ----------------------------------------------
+EDUCAT_USER = os.environ.get(
+    'EDUCAT_USER',
+    '<<** Coloque aqui seu usuário **>>'
+)
+EDUCAT_PASSWORD = os.environ.get(
+    'EDUCAT_PASSWORD',
+    '<<** Coloque aqui sua senha **>>'
+)
+EDUCAT_HOST = os.environ.get(
+    'EDUCAT_HOST',
+    'localhost'
+)
+# ----------------------------------------------
+
+def get_table(schema):
+    return LiteTable(schema, {
+            "host": EDUCAT_HOST,
+            "user": EDUCAT_USER,
+            "password": EDUCAT_PASSWORD,
+            "database": "educat"
+        })
