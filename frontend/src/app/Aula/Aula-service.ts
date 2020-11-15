290cc5cdb97a519dae435d87e7a2b7933149fef9
Júlio Cascalles <jcezarc@yahoo.com.br>
2020-11-15 11:20:42 -0300

    Ajustes de frontend

--- frontend/src/app/Aula/Aula-service.ts
+++ frontend/src/app/Aula/Aula-service.ts
@@ -3,7 +3,6 @@ import { Injectable } from "@angular/core";
 import { Observable } from "../../../node_modules/rxjs";
 import { AulaModel } from "./Aula-model";
 import { RespJsonFlask, BASE_PATH_SERVER } from "../app.api";
-import { AuthService } from '../shared/auth-service'
 
 const Aula_API = `${BASE_PATH_SERVER}/educat/Aula`
 
@@ -17,7 +16,6 @@ export class AulaService{
     allAulas():Observable<Response>{
         return this.http.get(
             Aula_API
-            ,new RequestOptions({headers: AuthService.header})
         )
     }
 
@@ -25,7 +23,6 @@ export class AulaService{
         this.http.put(
             Aula_API,
             JSON.stringify(newItem)
-            ,new RequestOptions({headers: AuthService.header})
         ).subscribe(
             resp => {
                 const obj:RespJsonFlask = (<RespJsonFlask>resp.json())
